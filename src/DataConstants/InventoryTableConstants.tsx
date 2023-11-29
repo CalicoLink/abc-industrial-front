5e6195bd5609ffa9a491fcbadf2a32e92eef1594
Ian Wascher <ianwascher98@gmail.com>
2023-11-29 17:52:12 -0600

    Modified Client interface to read from inventory

--- src/DataConstants/InventoryTableConstants.tsx
+++ src/DataConstants/InventoryTableConstants.tsx
@@ -3,7 +3,9 @@ import {InventoryTableRow} from '../DataObjects/InventoryTableInterface';
 export const INIT_INVENTORY_DATA: InventoryTableRow = {
     inventory_id: -1,
     inventory_name: "",
-    abc_client: -1,
+    abc_client: {
+        abc_client_id: -1
+    },
     storage_type: "",
     max_item_capacity: -1,
     address: "",
